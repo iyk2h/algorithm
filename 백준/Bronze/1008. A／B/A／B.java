ce64205b3b9eb894f726b3f63d9ce14b3f026c5f
youngkyun <iyk2h.dev@gmail.com>
2022-05-13 13:30:38 +0900

    [Bronze IV] Title: A/B, Time: 124 ms, Memory: 14540 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = a/b;
+        System.out.println(c);
+    }
+}
