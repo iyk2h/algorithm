16af8e0b4b7738a6fcf6a14868e16c367169b480
youngkyunIm <yee0230@naver.com>
2022-05-13 17:08:22 +0900

    1330

--- boj/no1330/Main.java
+++ boj/no1330/Main.java
@@ -1,2 +1,31 @@
-package boj.no1330;public class Main {
+package boj.no1330;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        String str;
+        while( (str = bf.readLine()) != null) {
+            st = new StringTokenizer(str);
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            if (A>B) {
+                System.out.println(">");
+            }
+            else if (A<B) {
+                System.out.println("<");
+            }
+            else {
+                System.out.println("==");
+            }
+        }
+
+    }
 }
