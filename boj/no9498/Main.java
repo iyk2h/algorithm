a15915c20a94dc269181a509afe89f399d64b209
youngkyunIm <yee0230@naver.com>
2022-05-15 19:28:55 +0900

    9498

--- boj/no9498/Main.java
+++ boj/no9498/Main.java
@@ -1,6 +1,27 @@
-package boj.no;
+package boj.no9498;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    //psvm+enter -> public static void main(String[] args){} 를 자동으로 작성
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        Integer score = Integer.parseInt(bf.readLine());
+
+            if (score >= 90) {
+                System.out.println("A");
+            }
+            else if (score >= 80) {
+                    System.out.println("B");
+            }
+            else if (score >= 70) {
+                System.out.println("C");
+            }
+            else if (score >= 60) {
+                System.out.println("D");
+            }
+            else
+                System.out.println("F");
+    }
 }
