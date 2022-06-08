64e8ad59a6218fc1441bfae65416c77e6b2b5795
youngkyun <iyk2h.dev@gmail.com>
2022-06-08 22:18:38 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 76 ms, Memory: 11580 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -5,31 +5,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
-
-        int str = Integer.parseInt(bf.readLine());
+        int c = Integer.parseInt(bf.readLine());
         int count = 0;
-        List<Character> cl = new ArrayList<Character>();
-        while (str > 0) {
-            boolean check[] = new boolean[26]; // 알파벳 26개, 기본 false 로 선언
-            String st = bf.readLine();
-            for (int i = 0; i < st.length(); i++) {
-                if (check[((int) (st.charAt(i)) - 'a')] == false) {
-                    check[((int) (st.charAt(i)) - 'a')] = true;
-                } else if (check[((int)(st.charAt(i))-'a')] == true){
-                    if (i > 0 ) {
-                        if ( st.charAt(i - 1) != st.charAt(i) ) {
-                            count--;
-                            break;
-                        }
+        while (c>0) {
+            if( check() == true) {
+                count++;
+            }
+            c--;
+        }
+        System.out.println(count);
+    }
+
+    public static boolean check() throws IOException {
+
+        boolean check[] = new boolean[26]; // 알파벳 26개, 기본 false 로 선언
+        String st = bf.readLine();
+
+        for (int i = 0; i < st.length(); i++) {
+            if (check[((int) (st.charAt(i)) - 'a')] == false) {
+                check[((int) (st.charAt(i)) - 'a')] = true;
+            } else if (check[((int)(st.charAt(i))-'a')] == true){
+                if (i > 0 ) {
+                    if ( st.charAt(i - 1) != st.charAt(i) ) {
+                        return false;
                     }
                 }
             }
-            count++;
-            str--;
         }
-        System.out.println(count);
+        return true;
     }
 }
