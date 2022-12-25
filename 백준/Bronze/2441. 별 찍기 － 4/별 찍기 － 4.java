0a577c0d06fca60b10af736d43e6957eed3cf3e8
youngkyun <iyk2h.dev@gmail.com>
2022-12-25 17:52:23 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 440 ms, Memory: 21064 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int input = Integer.parseInt(bf.readLine());
+
+        for (int i = input; i > 0; i--) {
+            printStar(i, input);
+            System.out.println();
+        }
+    }
+
+    public static void printStar(int input, int len) {
+        for (int i = 0; i < len - input; i++) {
+            System.out.printf("%s", " ");
+        }
+        for (int i = 0; i < input; i++) {
+            System.out.printf("%s", "*");
+        }
+    }
+}
