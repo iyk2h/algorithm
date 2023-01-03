90bea103a0f73168753408aa26cffc0fe43451fa
youngkyun <iyk2h.dev@gmail.com>
2023-01-03 22:58:06 +0900

    [Silver I] Title: 오르막 수, Time: 84 ms, Memory: 12532 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    static int[][] dp;
+    final static int divVal = 10_007;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        dp = new int[n + 1][10];
+
+        for (int i = 0; i < 10; i++) {
+            dp[1][i] = 1;
+        }
+
+        int result = 0;
+
+        for (int i = 0; i < 10; i++) {
+            result += L(n, i);
+        }
+        System.out.println(result % divVal);
+    }
+
+    static int L(int n, int i) {
+        if (n == 1) {
+            return dp[n][i];
+        }
+        if (dp[n][i] != 0) {
+            return dp[n][i];
+        }
+        if (i == 0) {
+            dp[n][i] = L(n - 1, i);
+        } else {
+            for (int j = i; j >= 0; j--) {
+                dp[n][i] += L(n - 1, j);
+            }
+        }
+
+        return dp[n][i] % divVal;
+    }
+}
