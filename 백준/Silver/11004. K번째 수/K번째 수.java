080799572bb5a1b640cda80d9bb127d3ba57bd12
youngkyun <iyk2h.dev@gmail.com>
2023-01-29 13:32:36 +0900

    [Silver V] Title: K번째 수, Time: 3372 ms, Memory: 664368 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.java
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -15,15 +17,18 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[N];
-
+//        int[] arr = new int[N];
+        ArrayList<Integer> arr = new ArrayList<>();
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+//            arr[i] = Integer.parseInt(st.nextToken());
+            arr.add(Integer.parseInt(st.nextToken()));
         }
+        
+        Collections.sort(arr);
 
-        Arrays.sort(arr);
+//        Arrays.sort(arr);
 
-        System.out.println(arr[K - 1]);
+        System.out.println(arr.get(K-1));
     }
 }
