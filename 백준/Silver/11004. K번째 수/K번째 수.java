8b2a3eaf8d724f5684e0b1abb2bb477ff0780565
youngkyun <iyk2h.dev@gmail.com>
2023-01-29 14:58:34 +0900

    [Silver V] Title: K번째 수, Time: 1768 ms, Memory: 502788 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.java
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -1,15 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] ar) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -17,18 +13,60 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-//        int[] arr = new int[N];
-        ArrayList<Integer> arr = new ArrayList<>();
+        int[] arr = new int[N];
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-//            arr[i] = Integer.parseInt(st.nextToken());
-            arr.add(Integer.parseInt(st.nextToken()));
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        
-        Collections.sort(arr);
 
-//        Arrays.sort(arr);
+        sort(arr);
 
-        System.out.println(arr.get(K-1));
+        System.out.println(arr[K - 1]);
     }
+
+    public static void sort(int[] a) {
+        m_pivot_sort(a, 0, a.length - 1);
+    }
+
+    private static void m_pivot_sort(int[] a, int lo, int hi) {
+        if (lo >= hi) {
+            return;
+        }
+        int pivot = partition(a, lo, hi);
+
+        m_pivot_sort(a, lo, pivot);
+        m_pivot_sort(a, pivot + 1, hi);
+    }
+
+    private static int partition(int[] a, int left, int right) {
+
+        int lo = left - 1;
+        int hi = right + 1;
+        int pivot = a[(left + right) / 2];
+
+        while (true) {
+            do {
+                lo++;
+            } while (a[lo] < pivot);
+
+            do {
+                hi--;
+            } while (a[hi] > pivot && lo <= hi);
+
+            if (lo >= hi) {
+                return hi;
+            }
+
+            swap(a, lo, hi);
+        }
+
+    }
+
+    private static void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
 }
