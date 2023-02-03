7d6f9ec1a4a42f6dbd0aa006656179bdf4e2ddcf
youngkyun <iyk2h.dev@gmail.com>
2023-02-03 09:03:00 +0900

    [Silver III] Title: 순열 사이클, Time: 500 ms, Memory: 55084 KB -BaekjoonHub

--- 백준/Silver/10451. 순열 사이클/순열 사이클.java
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.java
@@ -1,57 +1,44 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    public static ArrayList<ArrayList<Integer>> arr;
-    public static boolean[] visited;
-    public static int start;
-    public static int count;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int T = Integer.parseInt(br.readLine());
-        for (int i = 0; i < T; i++) {
-            arr = new ArrayList<>();
 
+        for (int i = 0; i < T; i++) {
             int N = Integer.parseInt(br.readLine());
 
-            visited = new boolean[N + 1];
-            for (int j = 0; j <= N; j++) {
-                arr.add(new ArrayList<>());
-            }
-
+            StringBuilder sb = new StringBuilder();
             StringTokenizer st = new StringTokenizer(br.readLine());
+
+            boolean[] visited = new boolean[N + 1];
+            int[] arr = new int[N + 1];
+            int count = 0;
+
             for (int j = 1; j <= N; j++) {
-                arr.get(j).add(Integer.valueOf(st.nextToken()));
+                arr[j] = Integer.parseInt(st.nextToken());
             }
 
-            count = 0;
             for (int j = 1; j <= N; j++) {
-                start = j;
-                if (!visited[j]) {
-                    dfs(j);
+                if (visited[j]) {
+                    continue;
+                }
+                int tmp = arr[j];
+                while (true) {
+                    if (visited[tmp]) {
+                        count++;
+                        break;
+                    }
+                    visited[tmp] = true;
+                    tmp = arr[tmp];
                 }
             }
-            System.out.println(count);
-        }
-
-    }
-
-    public static void dfs(int v) {
-        visited[v] = true;
-        for (int i = 0; i < arr.get(v).size(); i++) {
-            int x = arr.get(v).get(i);
-            if (x == start) {
-                count++;
-            }
-            if (!visited[x]) {
-                dfs(x);
-            }
+            sb.append(count);
+            System.out.println(sb);
         }
     }
 }
