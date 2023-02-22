e55b5f06069e4abfd99865bbb284efa0b9bb9739
youngkyun <iyk2h.dev@gmail.com>
2023-02-22 15:16:02 +0900

    [Silver V] Title: 숫자 카드, Time: 1016 ms, Memory: 141004 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -8,10 +8,10 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        StringBuilder sb = new StringBuilder();
+        
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        StringBuilder sb = new StringBuilder();
 
         int[] arr = new int[N];
 
@@ -23,36 +23,15 @@ public class Main {
 
         N = Integer.parseInt(br.readLine());
 
-        st = new StringTokenizer(br.readLine(), " ");
-
-
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            int target = Integer.parseInt(st.nextToken());
-            if (binarySearch(arr, target) == -1) {
-                sb.append(0);
-            } else {
+            if (Arrays.binarySearch(arr, Integer.parseInt(st.nextToken())) > -1) {
                 sb.append(1);
+            } else {
+                sb.append(0);
             }
             sb.append(" ");
         }
-        System.out.println(sb);
-    }
-
-    public static int binarySearch(int[] arr, int target) {
-        int si = 0;
-        int li = arr.length - 1;
-
-        while (si <= li) {
-            int mid = (si + li) / 2;
-
-            if (target == arr[mid]) {
-                return mid;
-            } else if (target > arr[mid]) {
-                si = mid + 1;
-            } else {
-                li = mid - 1;
-            }
-        }
-        return -1;
+        System.out.print(sb);
     }
 }
