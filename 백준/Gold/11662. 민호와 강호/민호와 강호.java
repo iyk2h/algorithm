d15fe4e618080b13af4686abd9867958f55e8de8
youngkyun <iyk2h.dev@gmail.com>
2023-02-22 15:38:44 +0900

    [Gold IV] Title: 민호와 강호, Time: 80 ms, Memory: 11636 KB -BaekjoonHub

--- 백준/Gold/11662. 민호와 강호/민호와 강호.java
+++ 백준/Gold/11662. 민호와 강호/민호와 강호.java
@@ -1,14 +1,12 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+ 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+        
         double aX1 = Double.parseDouble(st.nextToken());
         double aY1 = Double.parseDouble(st.nextToken());
         double aX2 = Double.parseDouble(st.nextToken());
@@ -17,28 +15,27 @@ public class Main {
         double cY1 = Double.parseDouble(st.nextToken());
         double cX2 = Double.parseDouble(st.nextToken());
         double cY2 = Double.parseDouble(st.nextToken());
-
+        
         int interval = 1000000;
-
+        
         double aDX = (aX2 - aX1) / interval;
         double aDY = (aY2 - aY1) / interval;
         double cDX = (cX2 - cX1) / interval;
         double cDY = (cY2 - cY1) / interval;
-
-        double min = getDistance(aX1, aY1, cX1, cY1);
-
-        for (int i = 1; i <= interval; i++) {
-            double tmp = getDistance(aX1 + aDX * i, aY1 + aDY * i, cX1 + cDX * i,
-                    cY1 + cDY * i);
-
-            if (tmp < min) {
-                min = tmp;
-            }
+        
+        double i = -1 * ((aX1 - cX1) * (aDX - cDX) + (aY1 - cY1) * (aDY - cDY)) / (Math.pow(aDX - cDX, 2) + Math.pow(aDY - cDY, 2));
+        
+        if(!Double.isNaN(i) && i < interval && i > 0) {
+            double min = getDistance(aX1 + aDX * i, aY1 + aDY * i, cX1 + cDX * i, cY1 + cDY * i);
+            System.out.println(min);
+        } else {
+            double min1 = getDistance(aX1, aY1, cX1, cY1);
+            double min2 = getDistance(aX2, aY2, cX2, cY2);
+            
+            System.out.println(Math.min(min1, min2));
         }
-
-        System.out.println(min);
     }
-
+    
     private static double getDistance(double x1, double y1, double x2, double y2) {
         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
