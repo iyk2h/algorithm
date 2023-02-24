1518e04c3976eca1ecc8de342dd3ca0e2693cee8
youngkyun <iyk2h.dev@gmail.com>
2023-02-24 17:10:17 +0900

    [level 0] Title: 짝수는 싫어요, Time: 5.77 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,13 +1,6 @@
+import java.util.stream.IntStream;
 class Solution {
     public int[] solution(int n) {
-        int[] answer = new int[n / 2 + n % 2] ;
-
-        int idx = 0;
-        for(int i = 1; i <= n; i = i + 2){
-            answer[idx] = i;
-            idx++;
-        }
-        
-        return answer;
+        return IntStream.rangeClosed(0, n).filter(value -> value % 2 == 1).toArray();
     }
 }
