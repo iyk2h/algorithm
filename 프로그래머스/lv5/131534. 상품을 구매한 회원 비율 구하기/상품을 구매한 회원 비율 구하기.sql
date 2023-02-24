c8852ba3f7ce88f66abb7887a978b48453f02f96
youngkyun <iyk2h.dev@gmail.com>
2023-02-24 16:13:59 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/lv5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,10 +1,25 @@
-SELECT YEAR, MONTH, COUNT(*) AS PUCHASED_USERS,
-	ROUND((COUNT(*)/ (SELECT COUNT(*)
-					FROM USER_INFO WHERE YEAR(JOINED) = 2021)), 1) AS PUCHASED_RATIO
-FROM (
-    SELECT DISTINCT YEAR(S.SALES_DATE) AS YEAR, MONTH(S.SALES_DATE) AS MONTH, U.USER_ID
-    FROM ONLINE_SALE S
-    JOIN USER_INFO U ON S.USER_ID = U.USER_ID AND YEAR(JOINED) = 2021
-) A
-GROUP BY YEAR, MONTH
-ORDER BY YEAR, MONTH
+select YEAR, MONTH, count(*) AS PUCHASED_USERS, round((count(*) / 
+                                                       (
+                                                           select count(*)
+                                                           from user_info
+                                                           where year(joined) = 2021
+                                                       )
+                                                      ), 1) as PUCHASED_RATIO
+from (select DISTINCT YEAR(s.SALES_DATE) as YEAR, MONTH(s.SALES_DATE) as MONTH, i.user_id
+      from ONLINE_SALE s
+      join USER_INFO i
+      on s.USER_ID = i.USER_ID and YEAR(i.JOINED) = "2021"
+     ) o
+group by YEAR, MONTH
+order by YEAR, MONTH;
+
+# SELECT YEAR, MONTH, COUNT(*) AS PUCHASED_USERS,
+# 	ROUND((COUNT(*)/ (SELECT COUNT(*)
+# 					FROM USER_INFO WHERE YEAR(JOINED) = 2021)), 1) AS PUCHASED_RATIO
+# FROM (
+#     SELECT DISTINCT YEAR(S.SALES_DATE) AS YEAR, MONTH(S.SALES_DATE) AS MONTH, U.USER_ID
+#     FROM ONLINE_SALE S
+#     JOIN USER_INFO U ON S.USER_ID = U.USER_ID AND YEAR(JOINED) = 2021
+# ) A
+# GROUP BY YEAR, MONTH
+# ORDER BY YEAR, MONTH
