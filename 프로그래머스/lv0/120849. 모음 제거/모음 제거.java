48997b348c234a1184968063b40687cb16c1258e
youngkyun <iyk2h.dev@gmail.com>
2023-02-26 14:06:32 +0900

    [level 0] Title: 모음 제거, Time: 9.41 ms, Memory: 92.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        for(int i = 0; i < my_string.length(); i++){
+            if(my_string.charAt(i) == 'i' || my_string.charAt(i) == 'o' || 
+               my_string.charAt(i) == 'e' || my_string.charAt(i) == 'u' || my_string.charAt(i) == 'a'){
+                continue;
+            }
+            answer += my_string.charAt(i);
+        }
+        
+        return answer;
+    }
+}
