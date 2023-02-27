7c3eb11b21830d13e8f82d0e939a7b2bce5e7921
youngkyun <iyk2h.dev@gmail.com>
2023-02-27 09:35:03 +0900

    [level 0] Title: 숫자 찾기, Time: 6.42 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,12 +1,7 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.*;
 
 class Solution {
     public int solution(int num, int k) {
-        List<Integer> list = Arrays.stream(String.valueOf(num).split(""))
-                .map(Integer::parseInt).collect(Collectors.toList());
-        
-        return list.indexOf(k) != -1 ? list.indexOf(k) + 1 : -1;
+        return ("-"+num).indexOf(String.valueOf(k));
     }
 }
