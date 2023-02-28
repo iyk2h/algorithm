9bb218a30802d0033edc2b011ec7c6a3a3466445
youngkyun <iyk2h.dev@gmail.com>
2023-02-28 10:53:36 +0900

    [level 0] Title: 공 던지기, Time: 0.02 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
@@ -1,13 +1,5 @@
 class Solution {
-    public int solution(int[] numbers, int k) {
-        int answer = 0;
-        
-        int idx = ((k - 1) * 2) % numbers.length;
-        
-        if(idx == 0){
-            idx = numbers.length - 1;
-        }
-        
-        return numbers[idx];
+    public int solution(int[] numbers, int k) {      
+        return ((k - 1) * 2) % numbers.length + 1;
     }
 }
