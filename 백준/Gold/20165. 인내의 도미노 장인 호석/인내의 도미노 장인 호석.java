7283d3eb996be6d4a014e18e7fb134e1f4274b73
youngkyun <iyk2h.dev@gmail.com>
2023-03-04 18:09:24 +0900

    [Gold V] Title: 인내의 도미노 장인 호석, Time: 348 ms, Memory: 22160 KB -BaekjoonHub

--- 백준/Gold/20165. 인내의 도미노 장인 호석/인내의 도미노 장인 호석.java
+++ 백준/Gold/20165. 인내의 도미노 장인 호석/인내의 도미노 장인 호석.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -22,34 +23,34 @@ public class Main {
         M = Integer.parseInt(st.nextToken()); // 열
         int R = Integer.parseInt(st.nextToken()); // 라운드 횟수
 
-        inputArr = new int[N][M]; // 입력 배열
-        visited = new char[N][M]; // 방문 여부 배열
+        inputArr = new int[N + 1][M + 1]; // 입력 배열
+        visited = new char[N + 1][M + 1]; // 방문 여부 배열
 
         // 입력 받기
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
+            for (int j = 1; j <= M; j++) {
                 inputArr[i][j] = Integer.parseInt(st.nextToken());
-                visited[i][j] = 'S';
+                visited[i][j] = 'S'; // 모든 칸을 방문하지 않은 상태로 초기화
             }
         }
 
         for (int i = 0; i < R; i++) {
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken()) - 1;
-            int y = Integer.parseInt(st.nextToken()) - 1;
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
             String direction = st.nextToken();
             attack(x, y, getDir(direction));
 
             st = new StringTokenizer(br.readLine());
-            x = Integer.parseInt(st.nextToken()) - 1;
-            y = Integer.parseInt(st.nextToken()) - 1;
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
             defense(x, y);
         }
 
         System.out.println(cnt);
-        for (int i = 0 ; i < N ; i++) {
-            for (int j = 0 ; j < M ; j++) {
+        for (int i = 1 ; i <= N ; i++) {
+            for (int j = 1 ; j <= M ; j++) {
                 System.out.print(visited[i][j]+" ");
             }
             System.out.println();
@@ -58,37 +59,40 @@ public class Main {
     }
 
     static void attack(int x, int y, int dir) {
-        if (visited[x][y] == 'F') {
+        if (visited[x][y] == 'F') { // 이미 방문한 칸이면 종료
             return;
         } else {
             int size = inputArr[x][y] - 1;
-            visited[x][y] = 'F';
-            cnt++;
+            visited[x][y] = 'F';  // 방문 표시
+            cnt++; // 방문 횟수 증가
 
             while (size > 0) {
                 int nx = x + dr[dir];
                 int ny = y + dc[dir];
-                if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
+                // 경계를 벗어난 경우 종료
+                if (nx < 1 || nx >= N + 1 || ny < 1 || ny >= M + 1) {
                     return;
                 }
-                if (visited[nx][ny] == 'F') {
-                    size--;
-                    x = nx;
-                    y = ny;
-                    continue;
-                }
                 size--;
-                cnt++;
-                visited[nx][ny] = 'F';
                 x = nx;
                 y = ny;
+                // 이미 방문한 칸
+                if (visited[nx][ny] == 'F') {
+                    continue;
+                }
+                // 방문하지 않은 칸
+                visited[nx][ny] = 'F';
 
-                int newSize = inputArr[nx][ny] - 1;
-                size = Math.max(newSize, size);
+                // 새로 방문한 곳과 기존 도미노 사이즈 비교
+                size = Math.max(size, inputArr[nx][ny] - 1);
+
+                //방문 횟수 증가
+                cnt++;
             }
         }
     }
 
+    // 방향 별 값
     static int getDir(String s) {
         if (s.equals("E")) {
             return 0;
@@ -102,9 +106,10 @@ public class Main {
         return 0;
     }
 
-    static void defense(int x, int y) {
-        if (visited[x][y] == 'F') {
-            visited[x][y] = 'S';
+    // 뒤집기
+    static void defense(int cr, int cc) {
+        if (visited[cr][cc] == 'F') {
+            visited[cr][cc] = 'S';
         } else {
             return;
         }
