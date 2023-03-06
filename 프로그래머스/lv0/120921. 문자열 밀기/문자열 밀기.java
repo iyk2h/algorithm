73116e3c42feddff9323f44a8dd42e45c0ebc948
youngkyun <iyk2h.dev@gmail.com>
2023-03-06 20:07:41 +0900

    [level 0] Title: 문자열 밀기, Time: 1.21 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,18 +1,6 @@
 class Solution {
     public int solution(String A, String B) {
-        int answer = 0;
-        String copy = A;
 
-        for (int i = 0; i < A.length(); i++) {
-            if (copy.equals(B)) {
-                return answer;
-            }
-
-            String a = copy.substring(copy.length() - 1);
-            copy = a + copy.substring(0, copy.length() - 1);
-            answer++;
-        }
-
-        return -1;
+        return (B+B).indexOf(A);
     }
 }
