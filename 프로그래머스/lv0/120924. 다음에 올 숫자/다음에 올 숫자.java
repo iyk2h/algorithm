716b1e856a5392f6263de2489cb1cb0e32cf17ef
youngkyun <iyk2h.dev@gmail.com>
2023-03-07 11:30:38 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.01 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] common) {
+        if((common[1] - common[0]) == (common[2] - common[1])){
+            return common[common.length - 1] + (common[1] - common[0]);
+        } else{
+            return common[common.length - 1] * (common[1] / common[0]);
+        }
+    }
+}
