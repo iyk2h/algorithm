41271b7519b2e4ebd55250b125cccfaffc0ced95
youngkyun <iyk2h.dev@gmail.com>
2023-03-23 10:06:20 +0900

    [level 2] Title: 구명보트, Time: 8.87 ms, Memory: 54.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/README.md
+++ 프로그래머스/lv2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885#) 
 
 ### 성능 요약
 
-메모리: 53.3 MB, 시간: 11.34 ms
+메모리: 54.2 MB, 시간: 8.87 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42885. 구명보트/구명보트.java
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -1,32 +1,24 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-
-        int[] arr = new int[241];
-
-        for(int i : people){
-            arr[i] += 1;
-        }
-
+        int n = people.length;
         Arrays.sort(people);
 
-        for(int i : people){
-            if (limit - i < i && arr[i] > 0){
-                arr[i]--;
-                answer++;
+        int i = n - 1;
+        int j = 0;
+
+        while (i > j) {
+            if (people[i] + people[j] <= limit) {
+                i--;
+                j++;
             }
-            for(int j = limit - i; j >= i; j--){
-                if(arr[j] > 0){
-                    answer++;
-                    arr[i]--;
-                    arr[j]--;
-                    break;
-                }
+            else {
+                i--;
             }
         }
 
-        return answer;
+        return answer = (n - j);
     }
 }
