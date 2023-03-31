348514cba1cca38c47e906093f7eecf7ec4b71fd
youngkyun <iyk2h.dev@gmail.com>
2023-03-31 15:39:34 +0900

    [level 2] Title: 주식가격, Time: 25.42 ms, Memory: 61.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] ans = new int[prices.length];
+        Stack<Integer> stack = new Stack();
+
+        for (int i = 0; i < prices.length; i++) {
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                ans[stack.peek()] = i - stack.peek();
+                stack.pop();
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            ans[stack.peek()] = prices.length - stack.peek() - 1;
+            stack.pop();
+        }
+
+        return ans;
+    }
+
+}
