684c14b7cb4d5850989fb0e7635286547eb6112c
youngkyun <iyk2h.dev@gmail.com>
2023-04-03 13:48:42 +0900

    [Silver II] Title: 키로거, Time: 744 ms, Memory: 156940 KB -BaekjoonHub

---
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.ListIterator;
+ 
+public class Main {
+    static LinkedList<Character> lnklist;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int TC = Integer.parseInt(br.readLine());
+        for(int i = 0; i < TC; i++) {
+            lnklist = new LinkedList<>();
+            ListIterator<Character> list = lnklist.listIterator();
+            String str = br.readLine();
+            
+            for(int j = 0; j < str.length(); j++) {
+                char c = str.charAt(j);
+                switch(c) {
+                    case '<' :
+                        if(list.hasPrevious()) {
+                            list.previous();
+                        }
+                        break;
+                    case '>' :
+                        if(list.hasNext()) {
+                            list.next();
+                        }
+                        break;
+                    case '-' :
+                        if(list.hasPrevious()) {
+                            list.previous();
+                            list.remove();
+                        }
+                        break;
+                    default : 
+                        list.add(c);
+                }
+            }
+            
+            StringBuilder sb = new StringBuilder();
+            for(char s : lnklist) {
+                sb.append(s);
+            }
+            System.out.println(sb.toString());
+        }
+    }
+}
