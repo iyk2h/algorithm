5c187ebcbb1b019897b8170c4c6908f11d7fbe09
youngkyun <iyk2h.dev@gmail.com>
2023-04-03 10:49:26 +0900

    [level 2] Title: 피로도, Time: 0.07 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/README.md
+++ 프로그래머스/lv2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946#) 
 
 ### 성능 요약
 
-메모리: 73 MB, 시간: 0.06 ms
+메모리: 73 MB, 시간: 0.07 ms
 
 ### 구분
 
--- 프로그래머스/lv2/87946. 피로도/피로도.java
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -1,22 +1,32 @@
 class Solution {
-    private int answer = 0;
-    private boolean[] visit;
-    public int solution(int k, int[][] dungeons) {
-        visit = new boolean[dungeons.length];
-        
-        dfs(0, k, dungeons);
-        
-        return answer;
-    }
-    
-    private void dfs(int depth, int k, int[][]dungeons){
-        for(int i = 0; i<dungeons.length; i++){
-            if(!visit[i]&& dungeons[i][0]<=k){
-                visit[i] = true;
-                dfs(depth+1, k-dungeons[i][1], dungeons);
-                visit[i] = false;
+
+    static boolean[] visited;
+    static int max = 0;
+
+    void dfs(int k, int[][] dungeons, int cnt) {
+        // 모든 케이스 순회
+        for (int i = 0; i < dungeons.length; i++) {
+            int a = dungeons[i][0];
+            int b = dungeons[i][1];
+
+            // 기존에 방분 확인, 최소 피로도 확인
+            if (visited[i] || k < a) {
+                continue;
             }
+
+            // 방문
+            visited[i] = true;
+            dfs(k - b, dungeons, cnt + 1);
+            // 다른 케이스를 위해 방문 취소
+            visited[i] = false;
         }
-        answer = Math.max(answer, depth);
+        max = Math.max(max, cnt);
+    }
+
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+
+        dfs(k, dungeons, 0);
+        return max;
     }
 }
