509fe065dc2a58db5d25e14bf2fbb17c491eb7b2
youngkyun <iyk2h.dev@gmail.com>
2023-04-05 21:00:41 +0900

    [level 2] Title: 더 맵게, Time: 1495.12 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -3,21 +3,24 @@ import java.util.*;
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
-
+        
+        //우선순위 큐 사용
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-
-        for (int i : scoville) {
+        for(int i : scoville){
             pq.add(i);
         }
-
+        
+        //스코빌 지수가 K 이상
         while(pq.peek() < K){
+            // 스코빌 지수를 K 이상으로 만들 수 없는 경우
             if(pq.size() == 1){
-                return -1;
+               return -1;
+            // else pq.add( poll() + poll() * 2 )
+            }else{
+                answer++;
+                pq.add(pq.poll() + pq.poll() * 2);
             }
-            pq.add(pq.poll() + (pq.poll() * 2 ));
-            answer++;
         }
-
         return answer;
     }
 }
