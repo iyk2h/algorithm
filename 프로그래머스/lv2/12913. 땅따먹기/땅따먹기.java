a5e9b2515b87f281b51b0d9588203a0cc899f38b
youngkyun <iyk2h.dev@gmail.com>
2023-04-05 21:08:46 +0900

    [level 2] Title: 땅따먹기, Time: 21.06 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.java
@@ -5,16 +5,15 @@ class Solution {
         int answer = 0;
 
         for(int i = 1; i < land.length; i++){
-            land[i][0] += Math.max(land[i-1][1], Math.max(land[i-1][2], land[i-1][3]));
-            land[i][1] += Math.max(land[i-1][0], Math.max(land[i-1][2], land[i-1][3]));
-            land[i][2] += Math.max(land[i-1][1], Math.max(land[i-1][0], land[i-1][3]));
-            land[i][3] += Math.max(land[i-1][1], Math.max(land[i-1][2], land[i-1][0]));
+            land[i][0] += Math.max(land[i - 1][1], Math.max(land[i - 1][2], land[i - 1][3]));
+            land[i][1] += Math.max(land[i - 1][0], Math.max(land[i - 1][2], land[i - 1][3]));
+            land[i][2] += Math.max(land[i - 1][1], Math.max(land[i - 1][0], land[i - 1][3]));
+            land[i][3] += Math.max(land[i - 1][1], Math.max(land[i - 1][2], land[i - 1][0]));
         }
+
+        int[] tmp = land[land.length-1];
+        Arrays.sort(tmp);
         
-        int[] arr = land[land.length-1];
-        
-        Arrays.sort(arr);
-        
-        return arr[arr.length - 1];
+        return tmp[tmp.length-1];
     }
 }
