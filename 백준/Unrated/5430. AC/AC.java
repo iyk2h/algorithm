deb6c2965c78441bcae305b52cd090d80d63023b
youngkyun <iyk2h.dev@gmail.com>
2023-04-11 22:49:19 +0900

    [Unrated] Title: AC, Time: 644 ms, Memory: 98316 KB -BaekjoonHub

---
+++ 백준/Unrated/5430. AC/AC.java
@@ -0,0 +1,94 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        ArrayDeque<Integer> deque;
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+
+            String command = br.readLine();
+            int n = Integer.parseInt(br.readLine());
+
+            st = new StringTokenizer(br.readLine(), "[],");
+
+            deque = new ArrayDeque<Integer>();
+
+            for (int i = 0; i < n; i++) {
+                deque.add(Integer.parseInt(st.nextToken()));
+            }
+
+            AC(command, deque);
+        }
+
+        System.out.println(sb);
+
+    }
+
+    public static void AC(String command, ArrayDeque<Integer> deque) {
+
+        boolean isRight = true;
+
+        for (char cmd : command.toCharArray()) {
+
+            if (cmd == 'R') {
+                isRight = !isRight;
+                continue;
+            }
+
+            if (isRight) {
+
+                if (deque.pollFirst() == null) {
+                    sb.append("error\n");
+                    return;
+                }
+
+            } else {
+
+                if (deque.pollLast() == null) {
+                    sb.append("error\n");
+                    return;
+                }
+            }
+        }
+
+        makePrintString(deque, isRight);
+
+    }
+
+    public static void makePrintString(ArrayDeque<Integer> deque, boolean isRight) {
+
+        sb.append('[');
+
+        if (deque.size() > 0) {
+
+            if (isRight) {
+
+                sb.append(deque.pollFirst());
+
+                while (!deque.isEmpty()) {
+                    sb.append(',').append(deque.pollFirst());
+                }
+            } else {
+                sb.append(deque.pollLast());
+
+                while (!deque.isEmpty()) {
+                    sb.append(',').append(deque.pollLast());
+                }
+            }
+        }
+
+        sb.append(']').append('\n');
+    }
+}
