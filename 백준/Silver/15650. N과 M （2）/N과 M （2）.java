70fbc6319aa20a64fabc9853751ad6f23f9e1e6f
youngkyun <iyk2h.dev@gmail.com>
2023-05-08 18:08:05 +0900

    [Silver III] Title: N과 M (2), Time: 76 ms, Memory: 11544 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -5,40 +5,46 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static boolean[] isused = new boolean[10];
-    static int n, s;
-
-    static int[] board = new int[10];
-
+    static StringBuffer sb = new StringBuffer();
+    static int n;
+    static int m;
+    static int[] board;
+    static boolean[] isUsed;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         n = Integer.parseInt(st.nextToken());
-        s = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        board = new int[n + 1];
+        isUsed = new boolean[n + 1];
 
         func(0);
+        System.out.println(sb);
     }
 
-    private static void func(int cur) {
-        if (cur == s) {
-            for (int i = 0; i < s; i++) {
-                System.out.print(board[i] + " ");
+    private static void func(int c) {
+        if (c == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(board[i]).append(" ");
             }
-            System.out.println();
+            sb.append("\n");
             return;
         }
-        int st = 1; // 시작점
-        if (cur != 0) {
-            st = board[cur - 1] + 1;
+
+        int st = 1;
+        if (c != 0) {
+            st = board[c - 1] + 1;
         }
+
         for (int i = st; i <= n; i++) {
-            if (!isused[i]) {
-                board[cur] = i;
-                isused[i] = true;
-                func(cur + 1);
-                isused[i] = false;
+            if (!isUsed[i]) {
+                isUsed[i] = true;
+                board[c] = i;
+                func(c + 1);
+                isUsed[i] = false;
             }
         }
     }
