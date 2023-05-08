3a22dee4952305c7ab6a51fdb8eb3dbb3a508d20
youngkyun <iyk2h.dev@gmail.com>
2023-05-08 15:04:32 +0900

    [Silver III] Title: N과 M (4), Time: 112 ms, Memory: 15664 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static boolean[] isused;
+    static int n, s;
+
+    static int[] board;
+
+    static StringBuffer sb = new StringBuffer();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        n = Integer.parseInt(st.nextToken());
+        s = Integer.parseInt(st.nextToken());
+
+        board = new int[n + 1];
+        isused = new boolean[n + 1];
+
+        func(0);
+        System.out.println(sb);
+    }
+
+    private static void func(int cur) {
+        if (cur == s) {
+            for (int i = 0; i < s; i++) {
+                sb.append(board[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        int st = 1;
+        if (cur != 0) {
+            st = board[cur - 1];
+        }
+        for (int i = st; i <= n; i++) {
+            board[cur] = i;
+            func(cur + 1);
+        }
+    }
+}
