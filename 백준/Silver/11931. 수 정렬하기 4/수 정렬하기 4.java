52830a519854e30f2364e70446f1606d7340594f
youngkyun <iyk2h.dev@gmail.com>
2023-05-16 14:49:36 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 828 ms, Memory: 131276 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n;
+    static int[] board;
+    static int[] tmp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+
+        board = new int[n];
+        tmp = new int[n];
+        for (int i = 0; i < n; i++) {
+            board[i] = Integer.parseInt(br.readLine());
+        }
+
+        merge_sort(0, n);
+
+        StringBuffer sb = new StringBuffer();
+        while (n-- > 0){
+            sb.append(board[n]).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static void merge(int s, int e) {
+        int mid = (s + e) / 2;
+        int lidx = s;
+        int ridx = mid;
+
+        for (int i = s; i < e; i++) {
+            if (ridx == e) {
+                tmp[i] = board[lidx++];
+            } else if (lidx == mid) {
+                tmp[i] = board[ridx++];
+            } else if (board[lidx] <= board[ridx]) {
+                tmp[i] = board[lidx++];
+            } else {
+                tmp[i] = board[ridx++];
+            }
+        }
+        for (int i = s; i < e; i++) {
+            board[i] = tmp[i];
+        }
+    }
+
+    private static void merge_sort(int s, int e) {
+        if (e == s + 1) {
+            return;
+        }
+        int mid = (s + e) / 2;
+        merge_sort(s, mid);
+        merge_sort(mid, e);
+        merge(s, e);
+    }
+}
