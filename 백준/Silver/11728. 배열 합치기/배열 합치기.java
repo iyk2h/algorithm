2e55022ef2c81474e2b918ed2826d0a62e2824da
youngkyun <iyk2h.dev@gmail.com>
2023-05-16 14:19:20 +0900

    [Silver V] Title: 배열 합치기, Time: 1244 ms, Memory: 332724 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -2,38 +2,52 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
+    static int n, m;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        ArrayList<Integer> al = new ArrayList<>();
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < A; i++) {
-            al.add(Integer.parseInt(st.nextToken()));
-        }
+        int[] f = new int[n];
+        int[] s = new int[m];
+        int[] answer = new int[n + m];
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < B; i++) {
-            al.add(Integer.parseInt(st.nextToken()));
+        for (int i = 0; i < n; i++) {
+            f[i] = Integer.parseInt(st.nextToken());
         }
 
-        Collections.sort(al);
-
-        StringBuilder sb = new StringBuilder();
-
-        for (int o : al) {
-            sb.append(o).append(" ");
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            s[i] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(sb);
+        int fi = 0;
+        int si = 0;
+        for (int i = 0; i < n + m; i++) {
+            if (si == m) {
+                answer[i] = f[fi++];
+            } else if (fi == n) {
+                answer[i] = s[si++];
+            } else if (f[fi] <= s[si]) {
+                answer[i] = f[fi++];
+            } else {
+                answer[i] = s[si++];
+            }
+        }
+
+        StringBuffer sb = new StringBuffer();
+        for (int j : answer) {
+            sb.append(j).append(" ");
+        }
+        System.out.print(sb);
     }
 }
