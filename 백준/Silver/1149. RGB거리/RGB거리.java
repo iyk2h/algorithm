d4c323f1119244cf9cb9211568ae6ee2c6cc0043
youngkyun <iyk2h.dev@gmail.com>
2023-05-19 21:40:28 +0900

    [Silver I] Title: RGB거리, Time: 88 ms, Memory: 12072 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+
+        int n = Integer.parseInt(br.readLine());
+        int total = 0;
+
+        int[][] dp = new int[n + 1][3];
+
+        for (int i = 1; i <= n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            dp[i][0] = Integer.parseInt(st.nextToken());
+            dp[i][1] = Integer.parseInt(st.nextToken());
+            dp[i][2] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 2; i <= n; i++) {
+            dp[i][0] += Math.min(dp[i - 1][1], dp[i - 1][2]);
+            dp[i][1] += Math.min(dp[i - 1][0], dp[i - 1][2]);
+            dp[i][2] += Math.min(dp[i - 1][1], dp[i - 1][0]);
+        }
+
+        total = Math.min(dp[n][0], Math.min(dp[n][1], dp[n][2]));
+
+        System.out.println(total);
+
+    }
+}
