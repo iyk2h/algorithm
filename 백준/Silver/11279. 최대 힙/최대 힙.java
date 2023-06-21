c9e90592683ab31640f25f2ebfe5b75b4a3f6e10
youngkyun <iyk2h.dev@gmail.com>
2023-06-21 21:24:07 +0900

    [Silver II] Title: 최대 힙, Time: 300 ms, Memory: 25236 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        while (n-- > 0) {
+            int i = Integer.parseInt(br.readLine());
+
+            if (i == 0) {
+                if (pq.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(pq.poll()).append("\n");
+                }
+            } else {
+                pq.offer(i);
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
