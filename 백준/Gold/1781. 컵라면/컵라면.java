1e5e91b2ca2fdcd5f121ec8db324f6cedd56b619
youngkyun <iyk2h.dev@gmail.com>
2023-06-21 22:16:00 +0900

    [Gold II] Title: 컵라면, Time: 808 ms, Memory: 76996 KB -BaekjoonHub

--- 백준/Gold/1781. 컵라면/컵라면.java
+++ 백준/Gold/1781. 컵라면/컵라면.java
@@ -11,7 +11,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuffer sb = new StringBuffer();
+
         StringTokenizer st;
 
         int sum = 0;
