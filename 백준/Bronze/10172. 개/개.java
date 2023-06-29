1492c8608a822b50cbc5b29342bcb60c0c4f5792
youngkyun <iyk2h.dev@gmail.com>
2023-06-29 22:14:32 +0900

    [Bronze V] Title: 개, Time: 76 ms, Memory: 11336 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("|\\_/|\n"
+                + "|q p|   /}\n"
+                + "( 0 )\"\"\"\\\n"
+                + "|\"^\"`    |\n"
+                + "||_/=\\\\__|");
+    }
+}
