a85e3c654a4be67aac8a6d9027e551f5127baa46
youngkyun <iyk2h.dev@gmail.com>
2023-07-20 19:05:47 +0900

    [Gold IV] Title: 플로이드, Time: 652 ms, Memory: 45196 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int INF = Integer.MAX_VALUE / 2;
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        int[][] board = new int[n + 1][n + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                board[i][j] = INF;
+                if (i == j) {
+                    board[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int v1 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            board[v1][v2] = Math.min(board[v1][v2], cost);
+        }
+
+        floyd(board, n);
+
+    }
+
+    private static void floyd(int[][] board, int n) {
+        for (int k = 1; k < n + 1; k++) {
+            for (int i = 1; i < n + 1; i++) {
+                for (int j = 1; j < n + 1; j++) {
+                    board[i][j] = Math.min(board[i][j], board[i][k] + board[k][j]);
+                }
+            }
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (board[i][j] == INF) {
+                    System.out.print(0);
+                } else {
+                    System.out.print(board[i][j]);
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
