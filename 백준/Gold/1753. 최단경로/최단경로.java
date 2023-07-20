43f3e1d9659e670dbd18e22962f09f0be265c535
youngkyun <iyk2h.dev@gmail.com>
2023-07-20 18:35:54 +0900

    [Gold IV] Title: 최단경로, Time: 1140 ms, Memory: 127552 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -2,13 +2,12 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static List<Node>[] graph;
+    static ArrayList<ArrayList<Node>> board = new ArrayList<>();
 
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,11 +17,8 @@ public class Main {
         int v = Integer.parseInt(st.nextToken());
         int e = Integer.parseInt(st.nextToken());
 
-        graph = new ArrayList[v + 1];
-
-
-        for (int i = 0; i <= v; i++) {
-            graph[i] = new ArrayList<>();
+        for (int i = 0; i < v + 1; i++) {
+            board.add(new ArrayList<>());
         }
 
         int start = Integer.parseInt(br.readLine());
@@ -33,15 +29,17 @@ public class Main {
             int v2 = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
 
-            graph[v1].add(new Node(v2, cost));
+            board.get(v1).add(new Node(v2, cost));
         }
 
         Dijkstra(v, start);
+
     }
 
     private static void Dijkstra(int n, int start) {
-        boolean[] check = new boolean[n + 1];
+        boolean[] visit = new boolean[n + 1];
         int[] dist = new int[n + 1];
+
         int INF = Integer.MAX_VALUE;
 
         Arrays.fill(dist, INF);
@@ -52,40 +50,34 @@ public class Main {
         pq.offer(new Node(start, 0));
 
         while (!pq.isEmpty()) {
-            int cur = pq.poll().index;
-
-            if (check[cur]) {
+            int curIndex = pq.poll().index;
+            if (visit[curIndex]) {
                 continue;
             }
+            visit[curIndex] = true;
 
-            check[cur] = true;
-
-            for (Node next : graph[cur]) {
-                if (dist[next.index] > dist[cur] + next.cost) {
-                    dist[next.index] = dist[cur] + next.cost;
-
+            for (Node next : board.get(curIndex)) {
+                if (dist[next.index] > dist[curIndex] + next.cost) {
+                    dist[next.index] = dist[curIndex] + next.cost;
                     pq.offer(new Node(next.index, dist[next.index]));
                 }
             }
         }
 
-        dist[0] = Integer.MIN_VALUE;
-
-        for (int i : dist) {
-            if (i == Integer.MIN_VALUE) {
-            } else if (i == INF) {
+        for (int i = 1; i < n + 1; i++) {
+            if (dist[i] == INF) {
                 System.out.println("INF");
             } else {
-                System.out.println(i);
+                System.out.println(dist[i]);
             }
         }
+
     }
 }
 
 class Node implements Comparable<Node> {
 
-    int index;
-    int cost;
+    int index, cost;
 
     public Node(int index, int cost) {
         this.index = index;
@@ -94,6 +86,6 @@ class Node implements Comparable<Node> {
 
     @Override
     public int compareTo(Node o) {
-        return Integer.compare(this.cost, o.cost);
+        return cost - o.cost;
     }
 }
