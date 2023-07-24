4785f43bcb47c537877e0aba3c189d7c53fca45f
youngkyun <iyk2h.dev@gmail.com>
2023-07-24 18:17:06 +0900

    [Gold IV] Title: 플로이드, Time: 524 ms, Memory: 45220 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -4,44 +4,47 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int INF = 987654321;
+    static int n, m;
+    static int INF = Integer.MAX_VALUE / 2;
 
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
 
-        int[][] board = new int[n + 1][n + 1];
+        int[][] arr = new int[n + 1][n + 1];
 
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < n + 1; j++) {
-                board[i][j] = INF;
+                arr[i][j] = INF;
                 if (i == j) {
-                    board[i][j] = 0;
+                    arr[i][j] = 0;
                 }
             }
         }
+
+        m = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < m; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
 
             int v1 = Integer.parseInt(st.nextToken());
             int v2 = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
+            int val = Integer.parseInt(st.nextToken());
 
-            board[v1][v2] = Math.min(board[v1][v2], cost);
+            arr[v1][v2] = Math.min(arr[v1][v2], val);
         }
 
-        floyd(board, n);
+        floyd(arr, n);
+
     }
 
-    private static void floyd(int[][] board, int n) {
+    private static void floyd(int[][] arr, int n) {
         for (int k = 1; k < n + 1; k++) {
             for (int i = 1; i < n + 1; i++) {
                 for (int j = 1; j < n + 1; j++) {
-                    if (board[i][j] > board[i][k] + board[k][j]) {
-                        board[i][j] = board[i][k] + board[k][j];
+                    if (arr[i][j] > arr[i][k] + arr[k][j]) {
+                        arr[i][j] = arr[i][k] + arr[k][j];
                     }
                 }
             }
@@ -49,12 +52,15 @@ public class Main {
 
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < n + 1; j++) {
-                if (board[i][j] == INF) {
-                    board[i][j] = 0;
+                if (arr[i][j] == INF) {
+                    arr[i][j] = 0;
                 }
-                System.out.print(board[i][j]+" ");
+                System.out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
+
     }
+
+
 }
