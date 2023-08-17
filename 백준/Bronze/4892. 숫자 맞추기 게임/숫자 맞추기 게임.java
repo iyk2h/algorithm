4d32bfb30accbfb53a441da1004f200eb10e455f
youngkyun <iyk2h.dev@gmail.com>
2023-08-17 20:20:21 +0900

    [Bronze III] Title: 숫자 맞추기 게임, Time: 140 ms, Memory: 13716 KB -BaekjoonHub

---
+++ 백준/Bronze/4892. 숫자 맞추기 게임/숫자 맞추기 게임.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count =1;
+        for (; ; ) {
+
+            int n = sc.nextInt();
+            if (n == 0) {
+                break;
+            }
+            if (n % 2 != 0) {
+                n = (((n * 3) + 1) / 2) * 3 / 9;
+                System.out.println(count + ". odd " + n);
+            } else {
+                n = (((n * 3) / 2) * 3) / 9;
+                System.out.println(count + ". even " + n);
+
+
+            }
+            count++;
+        }
+
+    }
+}
