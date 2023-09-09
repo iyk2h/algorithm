c5c82fff4954a37670151fad23ea55e7b26f6f6e
youngkyun <iyk2h.dev@gmail.com>
2023-09-09 16:52:49 +0900

    [level 2] Title: 타겟 넘버, Time: 0.43 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,18 +1,19 @@
 class Solution {
-    int answer = 0;
-    
+	int answer = 0;
     public int solution(int[] numbers, int target) {
-        
-        dfs(numbers, 0, target, 0);        
+        dfs(numbers, 0, target, 0);
         return answer;
     }
     
-    public void dfs(int[] numbers, int depth, int target, int sum){
-        if(depth == numbers.length){
-            if(target == sum) answer++;
+    
+    private void dfs(int[] numbers, int idx, int target, int sum){
+        if (numbers.length == idx){
+            if (target == sum) {
+                answer++;
+            }
         }else {
-            dfs(numbers, depth + 1, target, sum + numbers[depth]);
-            dfs(numbers, depth + 1, target, sum - numbers[depth]);  
+            dfs(numbers, idx + 1, target, sum + numbers[idx]);
+            dfs(numbers, idx + 1, target, sum - numbers[idx]);
         }
     }
 }
