e0f12a8115c5491f5887dbbdde618950e6f127e7
youngkyun <iyk2h.dev@gmail.com>
2023-09-11 14:14:58 +0900

    [level 3] Title: 단어 변환, Time: 0.02 ms, Memory: 66.8 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.java
@@ -1,36 +1,36 @@
 class Solution {
-    
-    boolean[] visit;
     int answer = 0;
-    
+    boolean[] visit;
     public int solution(String begin, String target, String[] words) {
         
         visit = new boolean[words.length];
         
         dfs(words, begin, target, 0);
-
+        
         return answer;
     }
     
-    private void dfs(String[] words, String cur, String target, int depth) {
+    private void dfs(String[] words, String cur, String target, int cnt) {
         if (cur.equals(target)) {
-            answer = depth;
+            answer = cnt;
             return;
         }
         
         for (int i = 0; i < words.length; i++) {
-            if (visit[i]) continue;
+            if (visit[i]) {
+                continue;
+            }
             
-            int k = 0;
+            int c = 0;
             for (int j = 0; j < cur.length(); j++) {
-                if (cur.charAt(j) != words[i].charAt(j)) {
-                    k++;
+                if (cur.charAt(j) == words[i].charAt(j)) {
+                    c++;
                 }
             }
             
-            if (k == 1) {
+            if (c == cur.length() - 1) {
                 visit[i] = true;
-                dfs(words, words[i], target, depth + 1);
+                dfs(words, words[i], target, cnt + 1);
                 visit[i] = false;
             }
         }
