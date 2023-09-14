1c935aeca8a5f495147416d890173b9e46774954
youngkyun <iyk2h.dev@gmail.com>
2023-09-14 22:05:24 +0900

    [level 1] Title: 모의고사, Time: 2.56 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/README.md
+++ 프로그래머스/lv1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=java) 
 
 ### 성능 요약
 
-메모리: 82.3 MB, 시간: 2.74 ms
+메모리: 77.3 MB, 시간: 2.56 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 100.0<br/>합계: 100.0 / 100.0
+Empty
 
 ### 문제 설명
 
--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -2,34 +2,40 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] one = {1, 2, 3, 4, 5}; // 5
-        int[] two = {2, 1, 2, 3, 2, 4, 2, 5}; // 8
-        int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}; // 10
-
-        int[] count = new int[3];
-
+        int[] answer = {};
+        
+        int[] a = new int[]{1,2,3,4,5}; // 5
+        int[] b = new int[]{2,1,2,3,2,4,2,5}; // 8
+        int[] c = new int[]{3,3,1,1,2,2,4,4,5,5}; // 10
+        
+        int[] abc = new int[]{0,0,0};
+        
         for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == one[i % 5]) {
-                count[0]++;
+            if (answers[i] == a[i % 5]) {
+                abc[0]++;
             }
-            if (answers[i] == two[i % 8]) {
-                count[1]++;
+            if (answers[i] == b[i % 8]) {
+                abc[1]++;
             }
-            if (answers[i] == three[i % 10]) {
-                count[2]++;
+            if (answers[i] == c[i % 10]) {
+                abc[2]++;
             }
         }
-
-        int max = Math.max(count[0], Math.max(count[1], count[2]));
-
+        
+        int max = 0;
+        for (int i : abc) {
+            if (max < i) {
+                max = i;
+            }
+        }
+        
         List<Integer> list = new ArrayList<>();
-
-        for (int i = 0; i < count.length; i++) {
-            if (count[i] == max) {
-                list.add(i + 1);
+        for (int i = 1; i < 4; i++) {
+            if (abc[i - 1] == max) {
+                list.add(i);
             }
         }
-
+        
         return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
