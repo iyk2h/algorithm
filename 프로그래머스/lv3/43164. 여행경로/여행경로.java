d7c73c908f93e8f579e88622413fe7561f01d558
youngkyun <iyk2h.dev@gmail.com>
2023-09-25 18:52:42 +0900

    [level 3] Title: 여행경로, Time: 14.12 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.java
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.java
@@ -4,30 +4,26 @@ class Solution {
     List<String> list;
     boolean[] visit;
     public String[] solution(String[][] tickets) {
-        String[] answer = {};
-        
-		list = new ArrayList<>();
         visit = new boolean[tickets.length];
+        list = new ArrayList<>();
         
         dfs("ICN", "ICN", tickets, 0);
         
         Collections.sort(list);
         
-        answer = list.get(0).split(" ");
-        
-		return answer;
+        return list.get(0).split(" ");
     }
     
-    private void dfs(String s, String route, String[][] tickets, int cnt) {
-        if (cnt == tickets.length) {
+    private void dfs(String s, String route, String[][] tickets, int depth) {
+        if (depth == tickets.length) {
             list.add(route);
             return;
         }
         
         for (int i = 0; i < tickets.length; i++) {
-            if (s.equals(tickets[i][0]) && !visit[i]) {
+            if (tickets[i][0].equals(s) && !visit[i]) {
                 visit[i] = true;
-                dfs(tickets[i][1], route + " " + tickets[i][1], tickets, cnt + 1);
+                dfs(tickets[i][1], route + " " + tickets[i][1], tickets, depth + 1);
                 visit[i] = false;
             }
         }
