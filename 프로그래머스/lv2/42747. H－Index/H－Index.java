42a370c0a6001a74a0ca932bc6679500a6360b7c
youngkyun <iyk2h.dev@gmail.com>
2023-09-27 23:07:30 +0900

    [level 2] Title: H-Index, Time: 0.34 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.java
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -6,10 +6,10 @@ class Solution {
         
         Arrays.sort(citations);
         
-        for(int i = 1 ; i <= citations.length; i++){
-            for(int j = 0 ; j < citations.length; j++){
-                if(i <= citations[j]){
-                    if(i == citations.length - j){
+        for (int i = 1; i <= citations.length; i++) {
+            for (int j = 0 ; j < citations.length; j++) {
+                if (i <= citations[j]) {
+                    if (i == citations.length - j) {
                         answer = i;
                         break;
                     }
