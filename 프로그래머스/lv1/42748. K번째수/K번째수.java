1063885c01b28fdebffdc2a5988b439e901437ea
youngkyun <iyk2h.dev@gmail.com>
2023-09-27 21:28:26 +0900

    [level 1] Title: K번째수, Time: 0.19 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -1,20 +1,19 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
-
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-
-        for (int i = 0; i < commands.length; i++) {
-
-            int[] nArr = Arrays.stream(array, commands[i][0] - 1, commands[i][1]).toArray();
-
-            Arrays.sort(nArr);
-
-            int value = nArr[commands[i][2] - 1];
-            answer[i] = value;
+        
+        for (int j = 0; j < commands.length; j++) {
+            int[] c = commands[j];
+            List<Integer> list = new ArrayList<>();
+            for (int i = c[0] - 1; i < c[1]; i++) {
+                list.add(array[i]);
+            }
+            Collections.sort(list);
+            answer[j] = list.get(c[2] - 1);
         }
-
+        
         return answer;
     }
 }
--- 프로그래머스/lv1/42748. K번째수/README.md
+++ 프로그래머스/lv1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java) 
 
 ### 성능 요약
 
-메모리: 76.4 MB, 시간: 1.87 ms
+메모리: 74.7 MB, 시간: 0.19 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 100.0<br/>합계: 100.0 / 100.0
+정확성: 100.0<br/>합계: 100.0 / 100.0
 
 ### 문제 설명
 
